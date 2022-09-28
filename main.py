6868e1e0999bc3af2a501421f43cf5d9443f0c6d
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:07:12 +0600

    Comment added

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ print(another_module.another_variable)
 from turtle import Turtle, Screen
 timmy = Turtle() #from turtle we fetch the blueprint or the class and name it timmy
 print(timmy) #<turtle.Turtle object at 0x0000014E02E720E0>
-my_screen = Screen()
+my_screen = Screen() #created an object called my_screen
 my_screen.canvheight #object.attribute calls the object and its attribute
+print(my_screen.canvheight) #prints 300
+my_screen.exitonclick() #object.method this method makes an action
