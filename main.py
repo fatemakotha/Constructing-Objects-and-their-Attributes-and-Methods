e20628d87d93479465cc11c8775b7c74de568d9c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:03:44 +0600

    object.attribute calls the object and its attribute

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import another_module
 
 print(another_module.another_variable)
 
-import turtle
-
-timmy = turtle.Turtle() #from turtle we fetch the blueprint or the class and name it timmy
+from turtle import Turtle, Screen
+timmy = Turtle() #from turtle we fetch the blueprint or the class and name it timmy
+print(timmy) #<turtle.Turtle object at 0x0000014E02E720E0>
+my_screen = Screen()
+my_screen.canvheight #object.attribute calls the object and its attribute
