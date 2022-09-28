46fc48cad2ddcb857e4b3c6835dd45ca2f7b291d
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:17:30 +0600

    timmy.forward(100) #turtle moves forward by a 100 paces

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ timmy = Turtle() #from turtle we fetch the blueprint or the class and name it ti
 print(timmy) #<turtle.Turtle object at 0x0000014E02E720E0>
 timmy.shape("turtle") #the object in the center becomes a turle
 timmy.color("blue") #turns the turtle blue
+timmy.forward(100) #turtle moves forward by a 100 paces
+
 my_screen = Screen() #created an object called my_screen
 my_screen.canvheight #object.attribute calls the object and its attribute
 print(my_screen.canvheight) #prints 300
