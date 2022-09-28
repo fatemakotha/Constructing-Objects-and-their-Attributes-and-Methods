871ba3eb3250c2f69a04cd0582bbf39b14c643a4
fatemakotha <fatema.alam.kotha@gmail.com>
2022-09-28 19:09:39 +0600

    timmy.shape("turtle") #the object in the center becomes a turle

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ print(another_module.another_variable)
 from turtle import Turtle, Screen
 timmy = Turtle() #from turtle we fetch the blueprint or the class and name it timmy
 print(timmy) #<turtle.Turtle object at 0x0000014E02E720E0>
+timmy.shape("turtle") #the object in the center becomes a turle
+
 my_screen = Screen() #created an object called my_screen
 my_screen.canvheight #object.attribute calls the object and its attribute
 print(my_screen.canvheight) #prints 300
-my_screen.exitonclick() #object.method this method makes an action
+my_screen.exitonclick() #object.method this method makes an action in this case makes a white screen appear
+
